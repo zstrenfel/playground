43947acfb72942a0d7d62f2a612e4b300e03fcc6
Zach Strenfel <zwstrenfel@gmail.com>
2016-04-09 05:09:39 -0700

    stuff:

--- assets/js/components/arena.js
+++ assets/js/components/arena.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import io from 'socket.io-client'
+import CountdownTimer from './countdownTimer'
 
 let socket = io(`http://localhost:8000`);
 
@@ -10,18 +11,24 @@ class Arena extends React.Component {
     super(props);
     this.state = {guess: "",
                   user:  "",
-                  opponent: "",
+                  opponent: "Geraldo",
                   room: "",
                   myScore: 0,
-                  opponentScore: 0}
+                  opponentScore: 0,
+                  timer: true,
+                  timeRemaing: 10000,
+                  isMounted: false,
+                }
     this.changeHandler = this.changeHandler.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTime = this.handleTime.bind(this);
+    this.handleTimeEnd = this.handleTimeEnd.bind(this);
   }
   componentWillMount() {
     let pathname = this.props.location.pathname.split('/');
     let roomNum = (pathname[pathname.length-2]);
     let name = (pathname[pathname.length-1]);
-    this.setState({room: roomNum, user: name});
+    this.setState({room: roomNum, user: name, isMounted: true});
   }
   componentDidMount() {
     //set room
@@ -37,6 +44,9 @@ class Arena extends React.Component {
       this.setState({opponent: opponent})
     })
   }
+  componentWillUnmount() {
+    this.setState({isMounted: false})
+  }
 
   handleSubmit(e) {
     e.preventDefault();
@@ -51,10 +61,26 @@ class Arena extends React.Component {
     this.setState({guess: e.target.value});
   }
 
+  handleTime(time) {
+    if (this.state.timeRemaing - time >= 1000) {
+      this.setState({timeRemaing: time});
+    }
+  }
+  handleTimeEnd() {
+    console.log('timer is donw');
+  }
+
   render() {
     return (
       <div className="arena">
         <h1>Welcome to the Arena, {this.state.user}!</h1>
+        <div className="timer-wrapper">
+          <CountdownTimer start={this.state.timer}
+                      initialTimeRemaining={10000}
+
+                      completeCallback={this.handleTimeEnd}
+                      />
+        </div>
 
         <form onSubmit={this.handleSubmit}>
           <input
@@ -63,6 +89,28 @@ class Arena extends React.Component {
           />
           <button>Submit</button>
         </form>
+        <div className="player-zone">
+          <div className="player">
+            <div className="my-points points">{this.state.myScore}</div>
+            <div className="profile">
+              <h3>{this.state.user}</h3>
+              <div className="sponsor">
+                Sponsoring: Saparkul
+                <img src={'./imgs/saparkul.jpg'} />
+              </div>
+            </div>
+          </div>
+          <div className="player">
+            <div className="opponent-points points">{this.state.opponentScore}</div>
+            <div className="profile">
+              <h3>{this.state.opponent}</h3>
+              <div className="sponsor">
+                Sponsoring: Saparkul
+                <img src={'./imgs/saparkul.jpg'} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
