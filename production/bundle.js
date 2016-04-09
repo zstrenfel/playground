98539ad68e5a0d11850db354526551ae68581264
Zach Strenfel <zwstrenfel@gmail.com>
2016-04-08 23:12:13 -0700

    added links to router

--- production/bundle.js
+++ production/bundle.js
@@ -172,6 +172,12 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactDom = require('react-dom');
 
+var _reactRouter = require('react-router');
+
+var _ArenaList = require('../../../ArenaList.json');
+
+var _ArenaList2 = _interopRequireDefault(_ArenaList);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -188,7 +194,7 @@ var Home = function (_React$Component) {
 
     var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(Home).call(this, props));
 
-    _this.state = { arenas: ["Arena 1", "Arena 2", "Arena 3"] };
+    _this.state = { arenas: _ArenaList2.default.arenas };
     return _this;
   }
 
@@ -196,12 +202,17 @@ var Home = function (_React$Component) {
     key: 'getArenas',
     value: function getArenas() {
       var arenaList = this.state.arenas.map(function (arena) {
+        var linkLocation = '/arena' + arena.altName;
         return _react2.default.createElement(
           'li',
           null,
-          ' ',
-          arena,
-          ' '
+          _react2.default.createElement(
+            _reactRouter.Link,
+            { to: '/arena/' + arena.altName },
+            ' ',
+            arena.arenaName,
+            ' '
+          )
         );
       });
       return arenaList;
